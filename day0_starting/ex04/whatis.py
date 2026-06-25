c513b73ec9d21d74b136abda569a616469c9dc12
gkorzecz <guillaume.korzeczek@gmail.com>
2026-06-25 22:18:38 +0200

    modified ex04

--- day0_starting/ex04/whatis.py
+++ day0_starting/ex04/whatis.py
@@ -4,7 +4,7 @@ import sys
 # number of argument give by : len(sys.argv), sys.argv[0] is program name
 argc = len(sys.argv)
 
-# use of sys.exit() insteand of defining a __main__
+# use of sys.exit() insteand of defining a __main__ and returning value
 
 if argc == 1:
     sys.exit(0)
@@ -13,14 +13,15 @@ try:
     assert argc <= 2, "more than one argument is provided"
 except AssertionError as e:
     print(f"AssertionError: {e}")
-    sys.exit(1)
+    sys.exit(0)
 
 # little going around the subject here, you can't assert that a string is an int if argv[1] can't already be converted to an int (raise an error)
+# casting is a good idea tho
 try:
     nb = int(sys.argv[1])
 except ValueError:
     print("AssertionError: argument is not an integer")
-    sys.exit(1)
+    sys.exit(0)
 
 
 if (nb % 2 == 0):
