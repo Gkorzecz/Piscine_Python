233ea7c967fb95c8a3dd05cc907baab084551a03
gkorzecz <guillaume.korzeczek@gmail.com>
2026-06-25 15:11:47 +0200

    finished ex02

--- day0_starting/ex02/find_ft_type.py
+++ day0_starting/ex02/find_ft_type.py
@@ -1,4 +1,19 @@
 def all_thing_is_obj(object: any) -> int:
-    print(type(object))
+
+    x = type(object)
+
+    if (x == list):
+        print("List :", x)
+    elif (x == tuple):
+        print("Tuple :", x)
+    elif (x == set):
+        print("Set :", x)
+    elif (x == dict):
+        print("Dict :", x)
+    elif (x == str):
+        print (object, "is in the kitchen :", x)
+    else:
+        print("Type not found")
+        
     return 42
 
