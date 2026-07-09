5ed02c2e30bb5d1616782306609edc67ad6f79f5
gkorzecz <guillaume.korzeczek@gmail.com>
2026-07-09 18:59:24 +0200

    finished ex02

--- day1_Array/ex02/load_image.py
+++ day1_Array/ex02/load_image.py
@@ -1,8 +1,32 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
-print('plt.imread()')
-img1 = plt.imread("France.jpg")
-print(type(img1))#np.array
-print(img1.shape)
-print('\n')
-print(img1)
+
+def verify_extension(path: str) -> bool:
+    file_extension = ["jpg", "jpeg"]
+
+    if '.' in path:
+        # [-1] = the last element in the list.
+        if path.split(".")[-1] in file_extension:
+            return True
+        else:
+            print("Error: wrong extension: only '.jpg' and '.jpeg' authorized")
+            return False
+    # no '.' in the filename, so no extension.
+    else:
+        print("Error: invalid filename: no extension")
+        return False
+
+
+def ft_load(path: str) -> np.ndarray:
+    try:
+        if verify_extension(path) is False:
+            return None
+
+        image = plt.imread(path)
+        print(f"The shape of image is: {image.shape}")
+        return image
+
+    except Exception as error:
+        print(f"Error: {error}")
+        return None
--- day1_Array/ex02/tester.py
+++ day1_Array/ex02/tester.py
@@ -1,4 +1,34 @@
 from load_image import ft_load
 
 
-print(ft_load("landscape.jpg"))
+print("\033[1mWrong extension (.png) :\033[0m")
+print(ft_load("wrong_extension.png"))
+print()
+
+print("\033[1mNo extension :\033[0m")
+print(ft_load("no_extension"))
+print()
+
+print("\033[1mDoes not exist :\033[0m")
+print(ft_load("does_not_exist.jpg"))
+print()
+
+print("\033[1mInvalid Image:\033[0m")
+print(ft_load("invalid_image.jpg"))
+print()
+
+print("\033[1mBlack vertical line (10x1, [0,0,0]) :\033[0m")
+print(ft_load("black_vert_line.jpg"))
+print()
+
+print("\033[1mWhite vertical line (10x1, [255, 255, 255]) :\033[0m")
+print(ft_load("white_vert_line.jpg"))
+print()
+
+print("\033[1mWhite vertical line (1x10, [255, 255, 255]) :\033[0m")
+print(ft_load("white_hori_line.jpg"))
+print()
+
+print("\033[1mBlack Square white diagonal(10x10) :\033[0m")
+print(ft_load("black_square_white_diag.jpg"))
+print()
